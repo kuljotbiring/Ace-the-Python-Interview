48aa628eae2a005c9d6be0e0022f83f9ae477cfc
Kuljot Biring <ksbiring1@gmail.com>
2022-05-15 16:24:09 -0700

    code to use hybrid inheritance

--- Python for Programmers/OOP in Python/multipleinheritance.py
+++ Python for Programmers/OOP in Python/multipleinheritance.py
@@ -1,29 +1,20 @@
-class Engine:  # Parent class
-    def setPower(self, power):
-        self.power = power
-
-
-class CombustionEngine(Engine):  # Child class inherited from Engine
+class CombustionEngine():
     def setTankCapacity(self, tankCapacity):
         self.tankCapacity = tankCapacity
 
 
-class ElectricEngine(Engine):  # Child class inherited from Engine
+class ElectricEngine():
     def setChargeCapacity(self, chargeCapacity):
         self.chargeCapacity = chargeCapacity
 
 # Child class inherited from CombustionEngine and ElectricEngine
-
-
 class HybridEngine(CombustionEngine, ElectricEngine):
     def printDetails(self):
-        print("Power:", self.power)
         print("Tank Capacity:", self.tankCapacity)
         print("Charge Capacity:", self.chargeCapacity)
 
 
 car = HybridEngine()
-car.setPower("2000 CC")
 car.setChargeCapacity("250 W")
 car.setTankCapacity("20 Litres")
 car.printDetails()
