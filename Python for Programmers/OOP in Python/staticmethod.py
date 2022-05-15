c3c0cee17d9c2cfc220bf6d0edabdebc35760abf
Kuljot Biring <ksbiring1@gmail.com>
2022-05-14 22:52:54 -0700

    add code for static method

---
+++ Python for Programmers/OOP in Python/staticmethod.py
@@ -0,0 +1,15 @@
+"""
+Static methods do not know anything about the state of the class, i.e., they cannot modify class attributes. The purpose
+of a static method is to use its parameters and produce a useful result.
+"""
+
+class BodyInfo:
+
+    @staticmethod
+    def bmi(weight, height):
+        return weight / (height**2)
+
+
+weight = 75
+height = 1.8
+print(BodyInfo.bmi(weight, height))
