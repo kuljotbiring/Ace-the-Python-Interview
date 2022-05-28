48b4439bb117dbbc39dd2cdb35087b7a3bcd866f
Kuljot Biring <ksbiring1@gmail.com>
2022-05-27 21:58:38 -0700

    add code demonstrating list construction

--- Python for Programmers/Data Structures and Algorithms in Python/Built-in Data Structures/Lists/listpractice.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Built-in Data Structures/Lists/listpractice.py
@@ -13,3 +13,36 @@ jon_snow = ["Jon Snow", "Winterfell", 30]
 print(jon_snow[2])
 jon_snow[2] += 3
 print(jon_snow[2])
+
+# using list with range
+num_seq = range(0, 10)  # A sequence from 0 to 9
+num_list = list(num_seq)  # The list() method casts the sequence into a list
+print(num_list)
+
+num_seq = range(3, 20, 3)  # A sequence from 3 to 19 with a step of 3
+print(list(num_seq))
+
+# nested lists
+world_cup_winners = [[2006, "Italy"], [2010, "Spain"],
+                     [2014, "Germany"], [2018, "France"]]
+print(world_cup_winners)
+
+# retrieving nest list information
+world_cup_winners = [[2006, "Italy"], [2010, "Spain"],
+                     [2014, "Germany"], [2018, "France"]]
+print(world_cup_winners[1])
+print(world_cup_winners[1][1])  # Accessing 'Spain'
+print(world_cup_winners[1][1][0])  # Accessing 'S'
+
+# merging lists using concatenation
+part_A = [1, 2, 3, 4, 5]
+part_B = [6, 7, 8, 9, 10]
+merged_list = part_A + part_B  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(merged_list)
+
+# merging lists using extent() function
+part_A = [1, 2, 3, 4, 5]
+part_B = [6, 7, 8, 9, 10]
+part_A.extend(part_B)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(part_A)
+
