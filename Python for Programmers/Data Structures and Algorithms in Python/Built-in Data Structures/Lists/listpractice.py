aff17959f846044f1e3f4593cd170bcc215ddaf5
Kuljot Biring <ksbiring1@gmail.com>
2022-05-27 21:16:37 -0700

    add code demonstrating mixed data types in a list

--- Python for Programmers/Data Structures and Algorithms in Python/Built-in Data Structures/Lists/listpractice.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Built-in Data Structures/Lists/listpractice.py
@@ -7,3 +7,9 @@ print(jon_snow[0])
 
 # Length
 print(len(jon_snow))
+
+# you can have mixed data types in a list
+jon_snow = ["Jon Snow", "Winterfell", 30]
+print(jon_snow[2])
+jon_snow[2] += 3
+print(jon_snow[2])
