47e009698c3edbf861c5e71f524cde27304f7835
Kuljot Biring <ksbiring1@gmail.com>
2022-05-28 21:59:49 -0700

    add code to demonstrate dictionary basics

---
+++ Python for Programmers/Data Structures and Algorithms in Python/Built-in Data Structures/Lists/dictionary.py
@@ -0,0 +1,34 @@
+# A dictionary stores key-value pairs, where each unique key is an index which holds the value associated with it.
+# dictionary is an unordered data structure, the order of the output will not necessarily match the order in which we
+# wrote the entries. Key-value pairs are accessed in a random or unordered manner.
+
+# creating a dictionary
+empty_dict = {}  # Empty dictionary
+print(empty_dict)
+
+phone_book = {"Batman": 468426,
+              "Cersei": 237734,
+              "Ghostbusters": 44678}
+print(phone_book)
+
+# creating a dictionary using the dict() constructor
+empty_dict = dict()  # Empty dictionary
+print(empty_dict)
+
+phone_book = dict(Batman=468426, Cersei=237734, Ghostbusters=44678)
+# Keys will automatically be converted to strings
+print(phone_book)
+
+# Alternative approach
+phone_book = dict([('Batman', 468426),
+                   ('Cersei', 237734),
+                   ('Ghostbusters', 44678)])
+print(phone_book)
+
+# how to access dictionary values
+phone_book = {"Batman": 468426,
+              "Cersei": 237734,
+              "Ghostbusters": 44678}
+print(phone_book["Cersei"])
+print(phone_book.get("Ghostbusters"))
+
