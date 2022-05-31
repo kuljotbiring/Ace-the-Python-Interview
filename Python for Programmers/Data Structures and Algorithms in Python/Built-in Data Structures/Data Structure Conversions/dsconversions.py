9eebca404a76a535f034481235e441a8f1f5068e
Kuljot Biring <ksbiring1@gmail.com>
2022-05-30 22:50:43 -0700

    add code for converting built-in data structures to list

---
+++ Python for Programmers/Data Structures and Algorithms in Python/Built-in Data Structures/Data Structure Conversions/dsconversions.py
@@ -0,0 +1,21 @@
+# The template for explicitly converting from one data structure to another is as follows:
+# destination_structure_name(source_structure_object)
+
+# converting to a list
+# we can convert a tuple, set, or dictionary to a list using the list() constructor. In the case of a dictionary, only
+# the keys will be converted to a list.
+star_wars_tup = ("Anakin", "Darth Vader", 1000)
+print(star_wars_tup)
+star_wars_set = {"Anakin", "Darth Vader", 1000}
+print(star_wars_set)
+star_wars_dict = {1: "Anakin", 2: "Darth Vader", 3: 1000}
+print(star_wars_dict)
+
+star_wars_list = list(star_wars_tup)  # Converting from tuple
+print(star_wars_list)
+
+star_wars_list = list(star_wars_set)  # Converting from set
+print(star_wars_list)
+
+star_wars_list = list(star_wars_dict)  # Converting from dictionary
+print(star_wars_list)
