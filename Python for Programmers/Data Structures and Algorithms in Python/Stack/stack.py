96e19ec8335026d7cb8e2d140cad0a2aa28bbad4
Kuljot Biring <ksbiring1@gmail.com>
2022-06-02 21:47:54 -0700

    add code to test stack implementation

--- Python for Programmers/Data Structures and Algorithms in Python/Stack/stack.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Stack/stack.py
@@ -12,3 +12,16 @@ class Stack:
 
     def pop(self):
         return self.items.pop()
+
+    def get_stack(self):
+        return self.items
+
+
+myStack = Stack()
+myStack.push("A")
+myStack.push("B")
+print(myStack.get_stack())
+myStack.push("C")
+print(myStack.get_stack())
+myStack.pop()
+print(myStack.get_stack())
