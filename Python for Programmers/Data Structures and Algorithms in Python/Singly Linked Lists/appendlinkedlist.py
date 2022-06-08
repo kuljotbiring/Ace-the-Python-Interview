b6c791ad8af48ca07b0621fc5090a5e687975672
Kuljot Biring <ksbiring1@gmail.com>
2022-06-07 18:55:29 -0700

    add code to print linked list

--- Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/appendlinkedlist.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/appendlinkedlist.py
@@ -8,6 +8,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def print_linked_list(self):
+        # initialize current node to head of the linked list
+        current_node = self.head
+        # use a while loop that keeps running if current_node is not equal to None
+        while current_node:
+            # print the data in the node and move to the next node
+            print(current_node.data)
+            current_node = current_node.next
+
     # append adds a node to the end of a linked list
     def append(self, data):
         new_node = Node(data)
@@ -22,3 +31,13 @@ class LinkedList:
             last_node = last_node.next
         # finally, make the last node point to the new node
         last_node.next = new_node
+
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+
+
+llist.print_linked_list()
