614edc5eb6c98acd32754ab096caed012dcf09c3
Kuljot Biring <ksbiring1@gmail.com>
2022-06-09 20:21:38 -0700

    add method to prepend to a linked list

---
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/prependlinkedlist.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_linked_list(self):
+        # initialize current node to head of the linked list
+        current_node = self.head
+        # use a while loop that keeps running if current_node is not equal to None
+        while current_node:
+            # print the data in the node and move to the next node
+            print(current_node.data)
+            current_node = current_node.next
+
+    # append adds a node to the end of a linked list
+    def append(self, data):
+        new_node = Node(data)
+        # address case if linked list is empty
+        if self.head is None:
+            self.head = new_node
+            return
+        # if list is not empty
+        last_node = self.head
+        # while there are other nodes - keep moving through linked list
+        while last_node.next:
+            last_node = last_node.next
+        # finally, make the last node point to the new node
+        last_node.next = new_node
+
+    def prepend(self, data):
+        # create a node based on data passed
+        new_node = Node(data)
+        # point the next of the new node to the current head of the linked list
+        new_node.next = self.head
+        # set head of the linked list equal to new_node
+        self.head = new_node
+
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+
+llist.prepend("D")
+
+llist.print_linked_list()
+
