9f5532903c1f8aed87adbfb9e8ec1a13b524e70f
Kuljot Biring <ksbiring1@gmail.com>
2023-02-04 11:23:56 -0800

    practice creating stacks

---
+++ Python for Programmers/Data Structures and Algorithms in Python/Stack/stackpractice.py
@@ -0,0 +1,36 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def get_stack(self):
+        return self.items
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+
+my_stack = Stack()
+my_stack.push("A")
+my_stack.push("B")
+my_stack.push("C")
+
+print(my_stack.get_stack())
+
+print(my_stack.peek())
+
+my_stack.pop()
+
+print(my_stack.peek())
+
+print(my_stack.get_stack())
