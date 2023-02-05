d70f95a4694e503b690d431c5c8c999ddd0f42f3
Kuljot Biring <ksbiring1@gmail.com>
2023-02-04 19:22:42 -0800

    add comments for SLL class

--- Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/sllclass.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/sllclass.py
@@ -1,11 +1,13 @@
 class Node:
 
     def __init__(self, data):
+        # store data and next node
         self.data = data
         self.next = None
 
 
 class LinkedList:
     def __init__(self):
+        # points to first node in linked list
         self.head = None
 
