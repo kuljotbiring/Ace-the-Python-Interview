7c2a75462ce48530ae7c5ccb3bca136a98516159
Kuljot Biring <ksbiring1@gmail.com>
2023-02-08 21:49:29 -0800

    add code for deleting LL head

---
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/deletionbyvalue.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        curr_node = self.head
+
+        while curr_node:
+            print(curr_node.data)
+            curr_node = curr_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+
+        while last_node.next:
+            last_node = last_node.next
+
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Previous Node does not exist")
+            return
+
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    # delete a Node
+    def delete_node(self, key):
+        curr_node = self.head
+
+        if curr_node and curr_node.data == key:
+            self.head = curr_node.next
+            curr_node = None
+            return
