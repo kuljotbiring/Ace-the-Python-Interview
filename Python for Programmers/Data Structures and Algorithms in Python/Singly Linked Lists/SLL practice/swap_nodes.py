07a0e99bdae35fe5fb7c3376613d6b769c0b20df
Kuljot Biring <ksbiring1@gmail.com>
2023-02-12 20:02:09 -0800

    add code for creating objects to implement methods

--- Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/swap_nodes.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/swap_nodes.py
@@ -140,3 +140,30 @@ class LinkedList:
 
         # swap what the curr Node next was pointing to
         curr_1.next, curr_2.next = curr_2.next, curr_1.next
+
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+
+print("Original List")
+llist.print_list()
+
+
+llist.swap_nodes("B", "C")
+print("Swapping nodes B and C that are not head nodes")
+llist.print_list()
+
+llist.swap_nodes("A", "B")
+print("Swapping nodes A and B where key_1 is head node")
+llist.print_list()
+
+llist.swap_nodes("D", "B")
+print("Swapping nodes D and B where key_2 is head node")
+llist.print_list()
+
+llist.swap_nodes("C", "C")
+print("Swapping nodes C and C where both keys are same")
+llist.print_list()
