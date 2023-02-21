0375de4fbafbb1f5638d323b4c710ad65803b7f8
Kuljot Biring <ksbiring1@gmail.com>
2023-02-20 20:18:56 -0800

    add code to instantiate linked list

--- Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/merged_sorted.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/merged_sorted.py
@@ -209,3 +209,22 @@ class LinkedList:
         # return head node of merged LL
         self.head = new_head
         return self.head
+
+
+llist_1 = LinkedList()
+llist_2 = LinkedList()
+
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(9)
+llist_1.append(10)
+
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(8)
+
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
