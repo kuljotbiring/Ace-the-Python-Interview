bf8ec5bbe1913babfe0dc14445c6ae04776388bb
Kuljot Biring <ksbiring1@gmail.com>
2023-02-22 20:55:20 -0800

    add code to initiate delete duplicate

--- Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/remove_duplicates.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/remove_duplicates.py
@@ -199,5 +199,21 @@ class LinkedList:
                 # update prev to curr
                 prev = curr
             # move forward in the LL
+            # can't use curr.next in case curr got deleted for duplicate
             curr = prev.next
 
+
+llist = LinkedList()
+llist.append(1)
+llist.append(6)
+llist.append(1)
+llist.append(4)
+llist.append(2)
+llist.append(2)
+llist.append(4)
+
+print("Original Linked List")
+llist.print_list()
+print("Linked List After Removing Duplicates")
+llist.remove_duplicates()
+llist.print_list()
