ac4cb8b43713baa8cf5f49d21bcc09f6abaec74d
Kuljot Biring <ksbiring1@gmail.com>
2023-02-24 17:13:34 -0800

    add code to run merge sorted list

--- Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/new_merge_sorted.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/SLL practice/new_merge_sorted.py
@@ -59,3 +59,21 @@ class LinkedList:
         # now return the list
         return tail.next
 
+
+llist_1 = LinkedList()
+llist_2 = LinkedList()
+
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(9)
+llist_1.append(10)
+
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(8)
+
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
