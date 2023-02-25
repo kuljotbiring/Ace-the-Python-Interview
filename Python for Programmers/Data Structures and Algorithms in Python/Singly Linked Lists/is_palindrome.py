32fe28e5fe9ac786c187fd7d31461d92f54931e0
Kuljot Biring <ksbiring1@gmail.com>
2023-02-25 14:42:07 -0800

    add code to check palindrom using LL and string

---
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/is_palindrome.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def is_palindrome_string(self):
+        s = ""
+        p = self.head
+
+        while p:
+            s += p.data
+            p = p.next
+        return s == s[::-1]
+
+
