aa2b82c2d8ca04e1b8911ec2abce59ceff752c9a
Kuljot Biring <ksbiring1@gmail.com>
2023-02-25 15:43:32 -0800

    add code to check palindrome using pointers

--- Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/is_palindrome.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Singly Linked Lists/is_palindrome.py
@@ -44,3 +44,60 @@ class LinkedList:
             p = p.next
 
         return True
+
+    def is_palindrome_ptr(self):
+        # check if LL is not empty
+        if self.head:
+            # assign variables to head of LL
+            p = self.head
+            q = self.head
+            # create an empty list
+            prev = []
+
+            i = 0
+            # move to end of LL while storing values in list
+            while q:
+                prev.append(q)
+                q = q.next
+                i += 1
+            # save the value of the last item
+            q = prev[i-1]
+
+            count = 1
+
+            # run while loop to half of list
+            while count <= i//2 + 1:
+                # go from back of list (-1) and front of LL
+                # items should be the same
+                if prev[-count].data != p.data:
+                    return False
+                p = p.next
+                count += 1
+            return True
+        else:
+            return True
+
+
+llist = LinkedList()
+llist.append("R")
+llist.append("A")
+llist.append("C")
+llist.append("E")
+llist.append("C")
+llist.append("A")
+llist.append("R")
+
+print(llist.is_palindrome_string())
+print(llist.is_palindrome_stack())
+print(llist.is_palindrome_ptr())
+
+print("\n\n")
+
+llist_2 = LinkedList()
+llist_2.append("A")
+llist_2.append("B")
+llist_2.append("C")
+
+print(llist_2.is_palindrome_string())
+print(llist_2.is_palindrome_stack())
+print(llist_2.is_palindrome_ptr())
