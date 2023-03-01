fe34993d949a64dbd1205d12baaa6492375edb68
Kuljot Biring <ksbiring1@gmail.com>
2023-02-28 21:51:52 -0800

    add comments for CLL detection

--- Python for Programmers/Data Structures and Algorithms in Python/Circular Linked Lists/test CLL.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Circular Linked Lists/test CLL.py
@@ -1,10 +1,14 @@
 def is_circular_linked_list(self, input_list):
+    # check if not empty
     if input_list.head:
         cur = input_list.head
+        # traverse till curr.next is None
         while cur.next:
             cur = cur.next
+            # if any node points back to head then CLL exists
             if cur.next == input_list.head:
                 return True
+        # if curr.next becomes None it means not CLL
         return False
     else:
         return False
