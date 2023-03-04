4c50a45e2369e61261f4b6927613cf1aae7fef12
Kuljot Biring <ksbiring1@gmail.com>
2023-03-04 11:30:54 -0800

    add code to instantiate DLL

--- Python for Programmers/Data Structures and Algorithms in Python/Doubly Linked Lists/append and prepend.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Doubly Linked Lists/append and prepend.py
@@ -45,3 +45,14 @@ class DoublyLinkedList:
         while curr:
             print(curr.data)
             curr = curr.next
+
+
+dllist = DoublyLinkedList()
+dllist.prepend(0)
+dllist.append(1)
+dllist.append(2)
+dllist.append(3)
+dllist.append(4)
+dllist.prepend(5)
+
+dllist.print_list()
