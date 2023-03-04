94d8d8b8e1cef7a1178bd17c2f5f45a55df4caff
Kuljot Biring <ksbiring1@gmail.com>
2023-03-03 21:35:45 -0800

    add code to append on DLL

--- Python for Programmers/Data Structures and Algorithms in Python/Doubly Linked Lists/append and prepend.py
+++ Python for Programmers/Data Structures and Algorithms in Python/Doubly Linked Lists/append and prepend.py
@@ -10,7 +10,20 @@ class DoublyLinkedList:
         self.head = None
 
     def append(self, data):
-        pass
+        # if LL is empty, make new Node the head Node
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            # make a new Node
+            new_node = Node(data)
+            curr = self.head
+            # iterate through the LL
+            while curr.next:
+                curr = curr.next
+            # make the next of curr the new Node
+            curr.next = new_node
+            # make the prev of new Node curr
+            new_node.prev = curr
 
     def prepend(self, data):
         pass
