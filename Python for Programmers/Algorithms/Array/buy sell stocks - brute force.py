f5ff04d5c70eeab1e98c87b4b8f1fd69ad4e33b7
Kuljot Biring <ksbiring1@gmail.com>
2023-03-25 23:32:23 -0700

    add brute force code for max profit for selling stocks

--- Python for Programmers/Algorithms/Array/buy sell stocks - brute force.py
+++ Python for Programmers/Algorithms/Array/buy sell stocks - brute force.py
@@ -15,18 +15,12 @@ before the day (index) indicating the selling price.
 
 
 def buy_and_sell_stock_once(prices):
-    buy = 0
-    sell = 1
     max_profit = 0
 
-    while sell < len(prices):
-        if prices[buy] < prices[sell]:
-            curr_profit = prices[sell] - prices[buy]
-            max_profit = max(max_profit, curr_profit)
-        else:
-            buy = sell
-        sell += 1
-
+    for i in range(len(prices) - 1):
+        for j in range(i + 1, len(prices)):
+            if prices[j] - prices[i]:
+                max_profit = prices[j] - prices[i]
     return max_profit
 
 
